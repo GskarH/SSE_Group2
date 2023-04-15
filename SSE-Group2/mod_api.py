1ea30e68a7c315d64795f207be2df1a7c41af330
Gowtham Baskar <109362329+GskarH@users.noreply.github.com>
2023-04-16 03:50:49 +0800

    Add files via upload

---
+++ SSE-Group2/mod_api.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, request, jsonify
+import libposts
+import libapi
+from jsonschema import validate, ValidationError
+
+
+mod_api = Blueprint('mod_api', __name__, template_folder='templates')
+
+# JSON schema for validating the request body for key creation
+key_schema = {
+    "type": "object",
+    "required": ["username", "password"],
+    "properties": {
+        "username": {"type": "string", "pattern": "^[a-z]+$"},
+        "password": {"type": "string"},
+    },
+    "additionalProperties": False,
+}
+
+# JSON schema for validating the request body for post creation
+post_schema = {
+    "type": "object",
+    "required": ["text"],
+    "properties": {
+        "text": {"type": "string"},
+    },
+    "additionalProperties": False,
+}
+
+
+@mod_api.route('/key', methods=['POST'])
+def do_key_create():
+    data = request.get_json()
+
+    try:
+        validate(data, key_schema)
+    except ValidationError:
+        return jsonify({'error': 'invalid schema', 'schema': key_schema}, 400)
+
+    key = libapi.keygen(data['username'], data['password'])
+
+    if key:
+        return jsonify({'key': key}), 200
+    else:
+        return jsonify({'error': 'invalid login'}), 200
+
+
+@mod_api.route('/post/<username>', methods=['GET'])
+def do_post_list(username):
+    posts = libposts.get_posts(username)
+
+    return jsonify(posts)
+
+
+@mod_api.route('/post', methods=['POST'])
+def do_post_create():
+
+    username = libapi.authenticate(request)
+
+    if not username:
+        return jsonify({'error': 'invalid authentication'}), 401
+
+    data = request.get_json()
+    try:
+        validate(data, post_schema)
+    except ValidationError:
+        return jsonify({'error': 'invalid schema', 'schema': post_schema}, 400)
+
+    text = data['text']
+    post_id = libposts.create_post(username, text)
+
+    return jsonify({'success': True, 'id': post_id}), 200
---
+++ SSE-Group2/mod_user.py
@@ -0,0 +1,87 @@
+from flask import Blueprint, render_template, redirect
+from flask import request, g, session, make_response, flash
+import libmfa
+import libuser
+import libsession
+
+mod_user = Blueprint('mod_user', __name__, template_folder='templates')
+
+
+@mod_user.route('/login', methods=['GET', 'POST'])
+def do_login():
+    # clear session before each login attempt
+    session.pop('username', None)
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        otp = request.form.get('otp')
+
+        # validate username and password
+        username = libuser.login(username, password)
+        if not username:
+            flash("Invalid user or password")
+            return render_template('user.login.mfa.html')
+
+        # check if MFA is enabled for this user
+        if libmfa.mfa_is_enabled(username):
+            # validate OTP
+            if not libmfa.mfa_validate(username, otp):
+                flash("Invalid OTP")
+                return render_template('user.login.mfa.html')
+
+        # create session for the authenticated user
+        response = make_response(redirect('/'))
+        response = libsession.create(response=response, username=username)
+        return response
+
+    return render_template('user.login.mfa.html')
+
+
+@mod_user.route('/create', methods=['GET', 'POST'])
+def do_create():
+    # clear session before each create attempt
+    session.pop('username', None)
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # validate username and password
+        if not username or not password:
+            flash("Please, complete username and password")
+            return render_template('user.create.html')
+
+        # create new user
+        libuser.create(username, password)
+        flash("User created. Please login.")
+        return redirect('/user/login')
+
+    return render_template('user.create.html')
+
+
+@mod_user.route('/chpasswd', methods=['GET', 'POST'])
+def do_chpasswd():
+    # validate session
+    if 'username' not in g.session:
+        return redirect('/user/login')
+
+    if request.method == 'POST':
+        password = request.form.get('password')
+        password_again = request.form.get('password_again')
+
+        # validate password complexity
+        if password != password_again:
+            flash("The passwords don't match")
+            return render_template('user.chpasswd.html')
+
+        if not libuser.password_complexity(password):
+            flash("The password doesn't comply with our \
+            complexity requirements")
+            return render_template('user.chpasswd.html')
+
+        # update password for the authenticated user
+        libuser.password_change(g.session['username'], password)
+        flash("Password changed")
+
+    return render_template('user.chpasswd.html')
