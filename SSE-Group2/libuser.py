b5872a0e0e3fcc1a7bc9cc13d708742150afb254
Gowtham Baskar <109362329+GskarH@users.noreply.github.com>
2023-04-16 16:40:40 +0800

    Update libuser.py

--- SSE-Group2/libuser.py
+++ SSE-Group2/libuser.py
@@ -25,11 +25,15 @@ def create(username, password):
 
     conn = sqlite3.connect('db_users.sqlite')
     c = conn.cursor()
-
-    c.execute(
-        "INSERT INTO users (username, password, failures, mfa_enabled, \
-        mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %
-        (username, password, 0, 0, ''))
+    
+##  Changed by SSE_Group2
+    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, \
+    mfa_secret) VALUES (?, ?, ?, ?, ?)", (username, password, 0, 0, ''))
+    
+   # c.execute(
+    #    "INSERT INTO users (username, password, failures, mfa_enabled, \
+     #   mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %
+      #  (username, password, 0, 0, ''))
 
     conn.commit()
     conn.close()
@@ -57,10 +61,13 @@ def password_change(username, password):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    c.execute(
-        "UPDATE users SET password = '{}' WHERE username = '{}'".format(
-            password,
-            username))
+##  Changed by SSE_Group2
+    c.execute("UPDATE users SET password = ? WHERE username = ?", (password, username))
+    
+    #c.execute(
+     #   "UPDATE users SET password = '{}' WHERE username = '{}'".format(
+      #      password,
+       #     username))
     conn.commit()
 
     return True
